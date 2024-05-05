0e7686d723e70476ada353f7ff93dc17c0361965
Munawar Jamil <munawar.minhas777@gmail.com>
2024-05-05 16:21:27 +0500

    updated cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -7,7 +7,7 @@ import Card from "react-bootstrap/Card";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function Cart() {
-  const cartItems = useSelector((state) => state.products);
+  const cartItems = useSelector((state) => state.cart);
   const dispatch = useDispatch();
 
   const removeFromCart = (product) => {
